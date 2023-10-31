06bd70759c5ecea608479c7cde80266079f798aa
Cristian Garcia <crisg@sas.upenn.edu>
2023-10-31 11:34:37 -0400

    refactored the code to create a function that generates a contact template and returns it as a list element

--- scripts/generateContact.js
+++ scripts/generateContact.js
@@ -1,33 +1,26 @@
 function generateContact(name, telephone, email, notes) {
-  //   const li = contactTemplate(name, telephone, email, notes);
+  // create a new li
+    const li = contactTemplate(name, telephone, email, notes);
+
+  // this will add a new li to the ul element
+  //Grab the ul list from the DOM
+  const ul = document.querySelector("ul");
+  // append the newly created li
+  ul.append(li);
+}
+
+function contactTemplate(name, telephone, email, notes) {
   const li = document.createElement("li");
   li.classList.add("single-contact");
 
-  // this will create a new li
   if (name && telephone && email) {
     li.innerHTML = `<h3>🌏</h3><p><strong>Name</strong>: ${name}</p>
     <p><strong>Tel:</strong></strong> ${telephone}</p>
     <p><strong>Email:</strong> ${email}</p>
     <p><strong>Notes:</strong> ${notes ? `${notes}` : `none`}</p>`;
-    //Grab the ul list from the DOM
-    const ul = document.querySelector("ul");
-    // append the newly created li
-    ul.append(li);
+
+    li.append(removeButton);
   }
+
+  return li;
 }
-
-// function contactTemplate(name, telephone, email, notes) {
-//   const li = document.createElement("li");
-//   li.classList.add("single-contact");
-//
-//   if (name && telephone && email) {
-//     li.innerHTML = `<h3>🌏</h3><p><strong>Name</strong>: ${name}</p>
-//     <p><strong>Tel:</strong></strong> ${telephone}</p>
-//     <p><strong>Email:</strong> ${email}</p>
-//     <p><strong>Notes:</strong> ${notes ? `${notes}` : `none`}</p>`;
-
-//     li.append(removeButton);
-//   }
-
-//   return li;
-// }
